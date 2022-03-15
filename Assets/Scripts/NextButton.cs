08f3825aeabc4ace0c9c8bf412ff2d14ff6e3cdd
iwalte01 <isabella.walters@tufts.edu>
2022-03-16 00:18:22 +0100

    button functionality for start -> instructions -> tutorial/level1
    still need text for the buttons - having trouble with this!

---
+++ Assets/Scripts/NextButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextButton : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("ABOUT TO CHANGE SCENES");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
